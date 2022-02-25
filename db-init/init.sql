53c096cb210dd08fa04139061fdb8428579a1655
eskokado <eskokado@gmail.com>
2022-02-25 12:25:03 -0300

    fix: add scripts initialize

---
+++ db-init/init.sql
@@ -0,0 +1,7 @@
+USE nodedb;
+
+CREATE TABLE `people` (
+`id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+`name` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
